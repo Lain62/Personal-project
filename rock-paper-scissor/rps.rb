b0a9c801ece9bde88d335a6d5021b95bff75f184
LainM <145809470+lain62@users.noreply.github.com>
2023-12-17 23:35:13 +0700

    Redoing the whole code, start with initializing
    Signed-off-by: LainM <145809470+Lain62@users.noreply.github.com>

--- rock-paper-scissor/rps.rb
+++ rock-paper-scissor/rps.rb
@@ -1,76 +1,17 @@
-# rock = 1
-# paper = 2
-# scissors = 3
+class RPS
+    def initialize
+        @player = {
+          selection: 'none',
+          score: 0
+        }
+        @computer = {
+          selection: '',
+          score: 0
+        }
 
-play = 1
-
-while play == 1 do
-
-get_computer_choice = rand(1..3)
-
-
-computer_selection ||= ""
-
-
-condition = 0 # draw
-
-puts "select: rock, paper, scissor"
-player_selection = gets.chomp.downcase
-
-puts "You've selected #{player_selection}!"
-
-case player_selection
-when "rock"
-    get_player_choice = 1
-when "paper"
-    get_player_choice = 2
-when "scissor"
-    get_player_choice = 3
-else
-    puts "error, that is not an option"
-    exit
-end
-
-case get_computer_choice
-when 1
-    computer_selection = "rock"
-when 2
-    computer_selection = "paper"
-when 3
-    computer_selection = "scissors"
-else
-	puts "ERROR"
-end
-
-if get_computer_choice == 1 && get_player_choice == 2
-    condition =  1 # win
-elsif get_computer_choice == 1 && get_player_choice == 3
-    condition = 2 # lost
-elsif get_computer_choice == 2 && get_player_choice == 1
-    condition = 1 # win
-elsif get_computer_choice == 2 && get_player_choice == 3
-    condition = 2 # lost
-elsif get_computer_choice == 3 && get_player_choice == 1
-    condition = 1 # win
-elsif get_computer_choice == 3 && get_player_choice == 2
-    condition = 2 # lost
-end
-
-puts "The computer chose #{computer_selection}!"
-
-case condition
-when 0
-    puts "It's a Draw!"
-when 1
-    puts "It's a Win!"
-when 2
-    puts "It's a lost!"
-else
-	puts "ERROR"
-end
-
-puts "try again? 1.yes 2.no (put the number)"
-play = gets.chomp.to_i
-   
+    end
 
 end
+
+game = RPS.new
+
